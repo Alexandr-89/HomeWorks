68390e8026bd5d04733c419bb0911208d89977fe
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-11-05 17:32:45 +0300

    added methods to Main class

--- src/by/overone/homework_21/Main.java
+++ src/by/overone/homework_21/Main.java
@@ -4,6 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Game.initialization();
+        Game.print();
+
+        while (true){
+
+
+            Game.movePlayer();
+            Game.print();
+            if (Game.checkWin(Game.PLAYER)){
+                System.out.println("You have won");
+                break;
+            }
+            if (Game.isFreeSpace()){
+                System.out.println("Draw");
+                break;
+            }
+            Game.moveComputer();
+            System.out.println();
+            Game.print();
+            if (Game.checkWin(Game.COMPUTER)){
+                System.out.println("Computer have won");
+                break;
+            }
+            if (Game.isFreeSpace()){
+                System.out.println("Draw");
+                break;
+            }
+        }
 
     }
 }
