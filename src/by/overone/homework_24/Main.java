dbcaf681e481e8796605d025d15b34d7fb243ede
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-11-12 21:47:10 +0300

    added Scanner in Main class

--- src/by/overone/homework_24/Main.java
+++ src/by/overone/homework_24/Main.java
@@ -1,4 +1,22 @@
 package by.overone.homework_24;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        while (true){
+            System.out.println("Your date of birth");
+            System.out.println("Enter month your of birth");
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNextLine()){
+                String mounth = scanner.nextLine();
+                System.out.println("Enter number your of birth");
+            }
+            if (scanner.hasNextInt()){
+                int number = scanner.nextInt();
+            }
+        }
+    }
 }
