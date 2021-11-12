6ae37947bc3826979723270cba2f7cef507539bb
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-11-12 21:38:19 +0300

    aded mounths and constructor in Mouth enum

--- src/by/overone/homework_24/Month.java
+++ src/by/overone/homework_24/Month.java
@@ -1,4 +1,22 @@
 package by.overone.homework_24;
 
 public enum Month {
+    JANYARY(31),
+    FEBRUARY(29),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DESEMBER(31);
+
+    int numberOfDays;
+
+    Month(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
 }
