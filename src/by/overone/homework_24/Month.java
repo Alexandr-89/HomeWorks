b663a1389470e34210f711b9186b075da6a774e6
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-11-12 22:23:51 +0300

    added method zodiacSign in Month enum

--- src/by/overone/homework_24/Month.java
+++ src/by/overone/homework_24/Month.java
@@ -14,9 +14,107 @@ public enum Month {
     NOVEMBER(30),
     DESEMBER(31);
 
-    int numberOfDays;
+    private int numberOfDays;
 
     Month(int numberOfDays) {
         this.numberOfDays = numberOfDays;
+    }
+
+    public int getNumberOfDays() {
+        return numberOfDays;
+    }
+
+    public static void zodiacSign(String mounth, int number){
+        try {
+            switch (valueOf(mounth.toUpperCase())){
+                case JANYARY:
+                    if (number>=0 && number<=20){
+                        System.out.println("Your zodiac sing - CAPRICORN");
+                    }else if (number>= 21 && number<= 31){
+                        System.out.println("Your zodiac sing - AQUARIUS ");
+                    }
+                    break;
+                case FEBRUARY:
+                    if (number>=0 && number<=19){
+                        System.out.println("Your zodiac sing - AQUARIUS");
+                    }else if (number>= 20 && number<= 29){
+                        System.out.println("Your zodiac sing - PISCES");
+                    }
+                    break;
+                case MARCH:
+                    if (number>=0 && number<=20){
+                        System.out.println("Your zodiac sing - PISCES");
+                    }else if (number>= 21 && number<= 31){
+                        System.out.println("Your zodiac sing - ARIES");
+                    }
+                    break;
+                case APRIL:
+                    if (number>=0 && number<=20){
+                        System.out.println("Your zodiac sing - ARIES");
+                    }else if (number>= 21 && number<= 30){
+                        System.out.println("Your zodiac sing - TAURUS");
+                    }
+                    break;
+                case MAY:
+                    if (number>=0 && number<=21){
+                        System.out.println("Your zodiac sing - TAURUS");
+                    }else if (number>= 22 && number<= 31){
+                        System.out.println("Your zodiac sing - GEMINI");
+                    }
+                    break;
+                case JUNE:
+                    if (number>=0 && number<=21){
+                        System.out.println("Your zodiac sing - GEMINI");
+                    }else if (number>= 22 && number<= 30){
+                        System.out.println("Your zodiac sing - CANCER");
+                    }
+                    break;
+                case JULY:
+                    if (number>=0 && number<=22){
+                        System.out.println("Your zodiac sing - CANCER");
+                    }else if (number>= 23 && number<= 31){
+                        System.out.println("Your zodiac sing - LEO");
+                    }
+                    break;
+                case AUGUST:
+                    if (number>=0 && number<=22){
+                        System.out.println("Your zodiac sing - LEO");
+                    }else if (number>= 23 && number<= 31){
+                        System.out.println("Your zodiac sing - VIRGO");
+                    }
+                    break;
+                case SEPTEMBER:
+                    if (number>=0 && number<=23){
+                        System.out.println("Your zodiac sing - VIRGO");
+                    }else if (number>= 24 && number<= 30){
+                        System.out.println("Your zodiac sing - LIBRA");
+                    }
+                    break;
+                case OCTOBER:
+                    if (number>=0 && number<=23){
+                        System.out.println("Your zodiac sing - LIBRA");
+                    }else if (number>= 24 && number<= 31){
+                        System.out.println("Your zodiac sing - SCORPIO");
+                    }
+                    break;
+                case NOVEMBER:
+                    if (number>=0 && number<=22){
+                        System.out.println("Your zodiac sing - SCORPIO");
+                    }else if (number>= 23 && number<= 30){
+                        System.out.println("Your zodiac sing - SAGITTARIUS");
+                    }
+                    break;
+                case DESEMBER:
+                    if (number>=0 && number<=21){
+                        System.out.println("Your zodiac sing - SAGITTARIUS");
+                    }else if (number>= 22 && number<= 31){
+                        System.out.println("Your zodiac sing - CAPRICORN");
+                    }
+                    break;
+            }
+        }catch (IllegalArgumentException e){
+            System.out.println("There is no such month");
+        }
+
     }
 }
