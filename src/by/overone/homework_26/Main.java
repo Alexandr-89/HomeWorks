e712c2cbfdfa2e8a947b9f3acdab4fa323d8ecee
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-11-26 14:35:29 +0300

    Created Supplier interface. Using Supplier wrote a method and implemented it.

--- src/by/overone/homework_26/Main.java
+++ src/by/overone/homework_26/Main.java
@@ -1,7 +1,20 @@
 package by.overone.homework_26;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        Supplier<String> supplier = () ->{
+            System.out.println("Enter string");
+            Scanner scanner = new Scanner(System.in);
+            String a = scanner.nextLine();
+            a = new StringBuilder(a).reverse().toString();
+            return a;
+        };
+
+        String rezult = supplier.string();
+        System.out.println(rezult);
     }
 }
---
+++ src/by/overone/homework_26/Supplier.java
@@ -0,0 +1,7 @@
+package by.overone.homework_26;
+
+public interface Supplier <T>{
+
+    T string ();
+
+}
