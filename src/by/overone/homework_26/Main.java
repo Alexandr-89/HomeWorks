2290e29d2158b1fe92117c93c14c50f6b29528bd
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-11-26 14:45:13 +0300

    Created Predicate interface. Using Predicate wrote a method and implemented it.

--- src/by/overone/homework_26/Main.java
+++ src/by/overone/homework_26/Main.java
@@ -16,5 +16,22 @@ public class Main {
 
         String rezult = supplier.string();
         System.out.println(rezult);
+
+
+        Predicate<String> dddd = q -> {
+            String[] a = q.split(", ");
+            Integer[] s = new Integer[a.length];
+            for (int i = 0; i<a.length; i++){
+                s[i] = Integer.parseInt(a[i]);
+                if (s[i]>=0){
+                    System.out.print(s[i]+",");
+                }else {
+                    continue;
+                }
+            }
+            return false;
+        };
+
+        dddd.test("1, 8, -3, 4, -9, 0, 7");
     }
 }
---
+++ src/by/overone/homework_26/Predicate.java
@@ -0,0 +1,7 @@
+package by.overone.homework_26;
+
+public interface Predicate <T>{
+
+    boolean test(T t);
+
+}
