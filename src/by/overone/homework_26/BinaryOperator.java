3e39a9bfd17dd5749e1fae211875a91c4aa64f1c
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-11-26 14:49:45 +0300

    Created BinaryOperator interface. Using BinaryOperator wrote a method and implemented it.

---
+++ src/by/overone/homework_26/BinaryOperator.java
@@ -0,0 +1,7 @@
+package by.overone.homework_26;
+
+public interface BinaryOperator <T> {
+
+    T aaaa(T t1, T t2);
+
+}
--- src/by/overone/homework_26/Main.java
+++ src/by/overone/homework_26/Main.java
@@ -1,5 +1,6 @@
 package by.overone.homework_26;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -17,6 +18,7 @@ public class Main {
         String rezult = supplier.string();
         System.out.println(rezult);
 
+        System.out.println();
 
         Predicate<String> dddd = q -> {
             String[] a = q.split(", ");
@@ -33,5 +35,21 @@ public class Main {
         };
 
         dddd.test("1, 8, -3, 4, -9, 0, 7");
+
+        System.out.println();
+
+        BinaryOperator<String> binaryOperator = (q1, q2) -> {
+            String[] a = q1.split("");
+            String[] b = q2.split("");
+            String[] c = new String[a.length];
+            for (int i =0; i<c.length; i++){
+                c[i] = a[i]+"-"+b[i];
+            }
+            String z = Arrays.toString(c);
+            System.out.println(z);
+            return z;
+        };
+
+        binaryOperator.aaaa("123456", "123456");
     }
 }
